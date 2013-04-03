c41605849d58764f4a4ddcbd1b4d84eb49d54fb9
horsley <imhorsley@gmail.com>
2013-04-03 12:22:49 +0800

    improve table view with ellipsis and fixed width

--- application/views/lnf_index.php
+++ application/views/lnf_index.php
@@ -1,34 +1,37 @@
 <div class="container" style="margin-top: 2em;">
     <div class="row-fluid" style="min-height: 400px">
         <?php if (!empty($items)): ?>
-        <table class="table  table-striped table-hover" >
+        <style>
+            #items td {overflow: hidden;white-space: nowrap;text-overflow: ellipsis;line-height: 1.6}
+        </style>
+        <table id="items" class="table table-striped table-hover" style="table-layout: fixed">
             <thead>
             <tr>
-                <th style="width: 40px">#</th>
-                <th>物品名称</th>
-                <th>物品简述</th>
-                <th style="width: 40px">校区</th>
-                <th>详细地点</th>
-                <th style="width: 80px">发布日期</th>
-                <th style="width: 40px">图片</th>
-                <th>保管地点/联系电话</th>
+                <th style="width: 5%">#</th>
+                <th style="width: 12%">物品名称</th>
+                <th style="width: 28%">物品简述</th>
+                <th style="width: 5%">校区</th>
+                <th style="width: 15%">详细地点</th>
+                <th style="width: 10%">发布日期</th>
+                <th style="width: 5%">图片</th>
+                <th style="width: 20%">保管地点/联系电话</th>
             </tr>
             </thead>
             <tbody>
             <?php foreach($items as $item): ?>
             <tr>
                 <td><?php echo $item['id']?></td>
-                <td><?php echo $item['name']?></td>
-                <td><?php echo $item['item_detail']?></td>
+                <td title="<?php echo $item['name']?>"><?php echo $item['name']?></td>
+                <td title="<?php echo $item['item_detail']?>"><?php echo $item['item_detail']?></td>
                 <td><?php echo $place[$item['place']]?></td>
-                <td><?php echo $item['place_detail']?></td>
+                <td title="<?php echo $item['place_detail']?>"><?php echo $item['place_detail']?></td>
                 <td><?php echo $item['pub_date']?></td>
                 <td><?php if (empty($item['pic_related'])) : ?>
                         无
                     <?php else: ?>
                         <a class="fancybox" href="<? echo base_url() . 'uploads/' . $item['pic_related'] ?>">查看</a>
                     <?php endif;?></td>
-                <td><?php echo $item['contact']?></td>
+                <td title="<?php echo $item['contact']?>"><?php echo $item['contact']?></td>
             </tr>
             <?php endforeach; ?>
 
